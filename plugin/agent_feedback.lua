9238292cceddab965ab610af03be3db360dda634
Gregory Cornelius <gc@gregorycornelius.com>
2026-04-20 08:13:18 -0400

    Use boolean

--- plugin/agent_feedback.lua
+++ plugin/agent_feedback.lua
@@ -1,7 +1,7 @@
-if vim.g.loaded_agent_feedback == 1 then
+if vim.g.loaded_agent_feedback then
   return
 end
 
-vim.g.loaded_agent_feedback = 1
+vim.g.loaded_agent_feedback = true
 
 require("agent_feedback").setup()
